0e11e469195e51e0d41a09aeaad1891c81395fe8
Haocheng Yang <alexyanghaocheng@gmail.com>
2023-06-17 23:06:31 +0800

    add choice node UI and behavior tree display

--- Source/ScoutsOdyssey/DialogueSystem/Tasks/MyBTTask_Choice.cpp
+++ Source/ScoutsOdyssey/DialogueSystem/Tasks/MyBTTask_Choice.cpp
@@ -2,4 +2,112 @@
 
 
 #include "MyBTTask_Choice.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Blueprint/UserWidget.h"
+#include "ScoutsOdyssey/DialogueSystem/DialogueComponent.h"
 
+
+UMyBTTask_Choice::UMyBTTask_Choice()
+{
+	NodeName = TEXT("Choice");
+
+	bNotifyTick = true;
+
+	DialogueComponent_BlackboardKey.AddObjectFilter(
+		this, GET_MEMBER_NAME_CHECKED(UMyBTTask_Choice, DialogueComponent_BlackboardKey),
+		UDialogueComponent::StaticClass());
+
+	ReplyIndex_BlackboardKey_Out.AddIntFilter(
+		this, GET_MEMBER_NAME_CHECKED(UMyBTTask_Choice, ReplyIndex_BlackboardKey_Out));
+}
+
+EBTNodeResult::Type UMyBTTask_Choice::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	TaskFinished = false;
+
+	// Get blackboard values
+	const UBlackboardComponent* BlackboardComponent = OwnerComp.GetBlackboardComponent();
+	DialogueComponent = Cast<UDialogueComponent>(
+		BlackboardComponent->GetValueAsObject(DialogueComponent_BlackboardKey.SelectedKeyName));
+
+	if (DialogueComponent)
+	{
+		DialogueComponent->SwitchToBubble(EBubble::One);
+
+		// Assign task finish delegate
+		Delegate_SetUp();
+
+		// Display Choice
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("DialogueComponent is Null: %s"), *GetClass()->GetName());
+	}
+
+	return EBTNodeResult::InProgress;
+}
+
+void UMyBTTask_Choice::TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	if (TaskFinished)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("BTTask Finished: %s"), *GetClass()->GetName());
+		OwnerComp.RequestExecution(EBTNodeResult::Succeeded);
+	}
+}
+
+
+FString UMyBTTask_Choice::GetStaticDescription() const
+{
+	FString ChoicesString;
+	for (int i = 0; i < Choices.Num(); i++)
+	{
+		ChoicesString += FString::Printf(TEXT("%d: "), i);;
+		ChoicesString += Choices[i].ToString();
+		ChoicesString += "\n";
+	}
+
+	return FString::Printf(TEXT("%s"), *ChoicesString);
+}
+
+
+void UMyBTTask_Choice::Delegate_SetUp()
+{
+	FOnDialogueEnd::FDelegate TaskFinish_Delegate;
+	TaskFinish_Delegate.BindUObject(this, &UMyBTTask_Choice::TaskFinish);
+	TaskFinish_DelegateHandle = DialogueComponent->OnChoiceFinish.Add(TaskFinish_Delegate);
+	UE_LOG(LogTemp, Warning, TEXT("TaskFinish Delegate Setup Successfully On OnChoiceFinish!"));
+}
+
+void UMyBTTask_Choice::TaskFinish()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Task Finished Called!"));
+	if (DialogueComponent)
+	{
+		// Remove Task Finish Delegate
+		DialogueComponent->OnSpeakFinish.Remove(TaskFinish_DelegateHandle);
+		TaskFinished = true;
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("Task cannot finish as Dialogue Component is Null"));
+	}
+}
+
+
+void UMyBTTask_Choice::InitializeFromAsset(UBehaviorTree& Asset)
+{
+	Super::InitializeFromAsset(Asset);
+
+	UBlackboardData* BBAsset = GetBlackboardAsset();
+	if (BBAsset)
+	{
+		DialogueComponent_BlackboardKey.ResolveSelectedKey(*BBAsset);
+		ReplyIndex_BlackboardKey_Out.ResolveSelectedKey(*BBAsset);
+	}
+	else
+	{
+		UE_LOG(LogBehaviorTree, Warning,
+		       TEXT("Can't initialize task: %s, make sure that behavior tree specifies blackboard asset!"), *GetName());
+	}
+}
--- Source/ScoutsOdyssey/DialogueSystem/Tasks/MyBTTask_Choice.h
+++ Source/ScoutsOdyssey/DialogueSystem/Tasks/MyBTTask_Choice.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "BehaviorTree/BTTaskNode.h"
+#include "..\DialogueEnums.h"
 #include "MyBTTask_Choice.generated.h"
 
 /**
@@ -13,5 +14,33 @@ UCLASS()
 class SCOUTSODYSSEY_API UMyBTTask_Choice : public UBTTaskNode
 {
 	GENERATED_BODY()
+	
+	/** initialize any asset related data */
+	virtual void InitializeFromAsset(UBehaviorTree& Asset) override;
+	
+public:
+	UMyBTTask_Choice();
+	
+private:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+	virtual void TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+	virtual FString GetStaticDescription() const override;
+
+	// Delegate Binding To Finish Task
+	void Delegate_SetUp();
+	void TaskFinish();
+	bool TaskFinished;
+	FDelegateHandle TaskFinish_DelegateHandle;
+
+	// Behavior Tree variables
+	class UDialogueComponent* DialogueComponent;
+	
+	// Behavior Tree editable
+	UPROPERTY(EditAnywhere, Category=Choice)
+	FBlackboardKeySelector DialogueComponent_BlackboardKey;
+	UPROPERTY(EditAnywhere, Category=Choice)
+	FBlackboardKeySelector ReplyIndex_BlackboardKey_Out;
+	UPROPERTY(EditAnywhere, Category=Choice, meta = (AllowPrivateAccess = true))
+	TArray<FText> Choices;
 	
 };
