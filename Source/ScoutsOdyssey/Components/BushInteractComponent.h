9f02c643bc9604b68c819aa3376bf3f383924a2f
Haocheng Yang <alexyanghaocheng@gmail.com>
2023-07-07 13:02:54 +0100

    add bushInteractComponent definition

---
+++ Source/ScoutsOdyssey/Components/BushInteractComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InteractComponentBase.h"
+#include "BushInteractComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SCOUTSODYSSEY_API UBushInteractComponent : public UInteractComponentBase
+{
+	GENERATED_BODY()
+	
+};
