656a7e6052770ebba2ceb6cbc5065f980127d743
Haocheng Yang <alexyanghaocheng@gmail.com>
2023-07-08 21:39:56 +0100

    Create BushInteractComponent, will test

--- Source/ScoutsOdyssey/Components/BushInteractComponent.cpp
+++ Source/ScoutsOdyssey/Components/BushInteractComponent.cpp
@@ -3,3 +3,47 @@
 
 #include "BushInteractComponent.h"
 
+#include "Math/UnitConversion.h"
+#include "ScoutsOdyssey/LoggingMacros.h"
+#include "ScoutsOdyssey/DialogueSystem/DialogueMeshActor.h"
+
+UBushInteractComponent::UBushInteractComponent()
+{
+	ADialogueMeshActor* OwnerActor = Cast<ADialogueMeshActor>(GetOwner());
+	if(OwnerActor)
+	{
+		StaticMeshComponent = OwnerActor->GetStaticMeshComponent();
+		StaticMeshComponent->SetRelativeScale3D(StartScale);
+	} else
+	{
+		LOG_ERROR("Cast failed!");
+	}	
+}
+
+void UBushInteractComponent::BeginPlay()
+{
+	TransformTimer = TransformTime;
+}
+
+
+
+void UBushInteractComponent::DoTask()
+{
+	//Clear it regardless
+	GetWorld()->GetTimerManager().ClearTimer(TimerHandle);
+	
+	GetWorld()->GetTimerManager().SetTimer(TimerHandle, FTimerDelegate::CreateLambda([&]()
+	{
+		
+		FVector CurScale = StaticMeshComponent->GetRelativeScale3D() +
+			TransformTimer/TransformTime * (EndScale - StartScale);
+		StaticMeshComponent->SetRelativeScale3D(CurScale);
+		
+		if(CurScale.Equals(EndScale, 0.001))
+		{
+			GetWorld()->GetTimerManager().ClearTimer(TimerHandle);	
+		}
+		TransformTimer -= 0.1f;
+		
+	}), 0.1f, true);
+}
--- Source/ScoutsOdyssey/Components/BushInteractComponent.h
+++ Source/ScoutsOdyssey/Components/BushInteractComponent.h
@@ -9,9 +9,25 @@
 /**
  * 
  */
-UCLASS()
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class SCOUTSODYSSEY_API UBushInteractComponent : public UInteractComponentBase
 {
 	GENERATED_BODY()
-	
+	UBushInteractComponent();
+
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void DoTask() override;
+
+	FVector StartScale = FVector(3);
+	FVector EndScale = FVector(1);
+	float TransformTime = 1;
+
+private:
+	UStaticMeshComponent* StaticMeshComponent;
+	FTimerHandle TimerHandle;
+	float TransformTimer;
 };
