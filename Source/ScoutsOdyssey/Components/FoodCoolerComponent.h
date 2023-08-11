70e657bca044d915eaee60b2f9a548b5f6b54ae3
Gregor McQueen <mcqueengregor@gmail.com>
2023-08-11 10:17:24 +0100

    Created new interaction components

---
+++ Source/ScoutsOdyssey/Components/FoodCoolerComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InteractComponentBase.h"
+#include "FoodCoolerComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SCOUTSODYSSEY_API UFoodCoolerComponent : public UInteractComponentBase
+{
+	GENERATED_BODY()
+	
+};
